c359e7773232892dbca7ad7e3d929dd114ae874d
gutovskii <volodimir.gutovskiy@gmail.com>
2021-12-25 21:21:14 +0200

    added some security from sql injection

--- Level 2/2.5-restful-mongodb/mysql/services/auth.ts
+++ Level 2/2.5-restful-mongodb/mysql/services/auth.ts
@@ -1,23 +1,26 @@
 import crypto from 'crypto'
 import db from "../db/connect"
-import { IUser } from "../interfaces/environment"
+import { IUser, findUserNameData, loginUserData, newUserData } from "../interfaces/environment"
 
 export const loginService = async (userData: IUser) => {
-    const sql = `SELECT * FROM users WHERE login = '${userData.login}' AND pass = '${userData.pass}'`
-    const [rowsGetUser]: any = await (await db).query(sql)
+    const sql = `SELECT * FROM users WHERE login = ? AND pass = ?`
+    const loginUserData: loginUserData = [userData.login, userData.pass]
+    const [rowsGetUser]: any = await db.query(sql, loginUserData)
     if (!rowsGetUser.length) return undefined
     const user = rowsGetUser
     return user
 }
 
 export const registerService = async (userData: IUser) => {
-    const sqlGetUser = `SELECT * FROM users WHERE login = '${userData.login}'`
-    const [rowsGetUser]: any = await (await db).query(sqlGetUser)
+    const sqlGetUser = `SELECT * FROM users WHERE login = ?`
+    const findUserNameData: findUserNameData = [userData.login]
+    const [rowsGetUser]: any = await db.query(sqlGetUser, findUserNameData)
     if (rowsGetUser.length) return undefined
     
     userData._id = crypto.randomBytes(16).toString('hex')
     const sqlRegister = `INSERT INTO users(_id, login, pass) VALUES(?, ?, ?)`
-    const [rowsCreateUser] = await (await db).query(sqlRegister, [userData._id, userData.login, userData.pass])
+    const newUserData: newUserData = [userData._id, userData.login, userData.pass]
+    const [rowsCreateUser] = await db.query(sqlRegister, newUserData)
     const newUser = rowsCreateUser
     return newUser
 }
